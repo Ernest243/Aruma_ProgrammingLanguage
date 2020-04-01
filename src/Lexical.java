fe059bcb470fd003a132caa59b33c9fa911c4cad
Ernest <emudjambere@gmail.com>
2020-04-01 05:45:22 -0400

    Update on checking operation code

--- src/Lexical.java
+++ src/Lexical.java
@@ -1,6 +1,8 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Lexical 
 {
@@ -15,46 +17,84 @@ public class Lexical
 	 */
 	
 	public static void readSource(String name) throws FileNotFoundException
-	{
-		// Initialize the Scanner & File utility to read from the user
-		Scanner input = new Scanner(new File(name));
+	{	
+		// Initialize the HashMap
+		HashMap<Integer, String> dataType = new HashMap<Integer, String>();
 		
-		//Initialize variable that will hold each line of the file
-		String line = "";
+		// Registering different code OP in the hash
+		dataType.put(1, "add_OP");
+		dataType.put(2, "sub_OP");
+		dataType.put(3, "div_OP");
+		dataType.put(4, "mul_OP");
+		dataType.put(5, "mod_OP"); 
 		
-		// Looping through the file
-		while(input.hasNext()) 
+		try 
 		{
-			// Save the first line in the line variable
-			line = input.nextLine();
+			// Initialize the Scanner & File utility to read from the user
+			Scanner input = new Scanner(new File(name));
 			
+			//Initialize variable that will hold each line of the file
+			String line = "";
 			
-			//Array that will hold each word after removing white space
-			String[] tokens = line.split(" ");
-			
-			// Looping through the array
-			for(String toke : tokens) 
+			// Looping through the file
+			while(input.hasNext()) 
 			{
-				// Check for String
-				if(toke.matches(".*[a-zA-Z]")) 
+				// Save the line in the line variable
+				line = input.nextLine();
+				
+				
+				//Array that will hold each word after removing white space
+				String[] lexemes = line.split(" ");
+				
+				// Looping through the array
+				for(String lex : lexemes) 
 				{
-					System.out.println(toke + " => " + "String");
+					// Check for String
+					if(lex.matches(".*[a-zA-Z]")) 
+					{
+						System.out.println(lex + " => " + "String");
+					}
+					
+					// Check for Integer
+					else if(lex.matches(".*[0-9]")) 
+					{
+						System.out.println(lex + " => " + "Integer");
+					}
+					
+					// Check for special character
+					else if(lex.matches(".*[$&+,:;=\\\\?@#|/'<>.^*\"{}()\\[\\]%!-]")) 
+					{
+						if(lex.matches(".*[\\+]")) 
+						{
+							System.out.println(lex + " => " + "ADD_OP");
+						}
+						else if(lex.matches(".*[-]")) 
+						{
+							System.out.println(lex + " => " + "SUB_OP");
+						}
+						else if(lex.matches(".*[\\*]")) 
+						{
+							System.out.println(lex + " => " + "MUL_OP");
+						}
+						else if(lex.matches(".*[/]")) 
+						{
+							System.out.println(lex + " => " + "DIV_OP");
+						}
+						else if(lex.matches(".*[%]")) 
+						{
+							System.out.println(lex + " => " + "MOD_OP");
+						}
+						else
+							System.out.println(lex + " => " + "Special Character");
+					}
 				}
 				
-				// Check for Integer
-				else if(toke.matches(".*[0-9]")) 
-				{
-					System.out.println(toke + " => " + "Integer");
-				}
-				
-				// Check for special character
-				else if(toke.matches(".*[$&+,:;=\\\\?@#|/'<>.^*\"{}()\\[\\]%!-]")) 
-				{
-					System.out.println(toke + " => " + "Special Character");
-				}
 			}
-			
 		}
 		
+		catch(FileNotFoundException e) 
+		{
+			System.out.println("File not found!");
+		}
 	}
 }
