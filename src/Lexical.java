26af87dc3bdebcec7eec52023a544c38c78ed53b
Ernest <emudjambere@gmail.com>
2020-04-01 07:34:11 -0400

    Update func

--- src/Lexical.java
+++ src/Lexical.java
@@ -1,32 +1,34 @@
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Lexical 
 {
-	public static void main(String[] args) throws FileNotFoundException
+	public static void main(String[] args) throws IOException
 	{
 		// Testing if the function is working...
-		readSource("test_2");
+		readSource("arumaTest");
 	}
 	
 	/*
-	 * This function will read the source file input from the user
+	 * This function will read the source file input from the user, 
+	 * generate tokens & print them out to a new file that will be returned
+	 * as the output of this function.
 	 */
 	
-	public static void readSource(String name) throws FileNotFoundException
+	public static FileWriter readSource(String name) throws IOException
 	{	
-		// Initialize the HashMap
-		HashMap<Integer, String> dataType = new HashMap<Integer, String>();
+		/*
+		 * Initializing output file containing tokens to be read by the syntax analyzer
+		 */
 		
-		// Registering different code OP in the hash
-		dataType.put(1, "add_OP");
-		dataType.put(2, "sub_OP");
-		dataType.put(3, "div_OP");
-		dataType.put(4, "mul_OP");
-		dataType.put(5, "mod_OP"); 
+		FileWriter tokens = new FileWriter("outputToken.txt");
+		PrintWriter writeToFile = new PrintWriter(tokens);
+		ArrayList<String> tokensOutput = new ArrayList<String>();
 		
 		try 
 		{
@@ -42,23 +44,53 @@ public class Lexical
 				// Save the line in the line variable
 				line = input.nextLine();
 				
-				
 				//Array that will hold each word after removing white space
 				String[] lexemes = line.split(" ");
 				
 				// Looping through the array
 				for(String lex : lexemes) 
 				{
-					// Check for String
+					// Check for String Data Type
 					if(lex.matches(".*[a-zA-Z]")) 
 					{
-						System.out.println(lex + " => " + "String");
+						if(lex.matches("[Litteral]*")) 
+						{
+							tokensOutput.add("(LITTERAL)");
+						}
+						
+						if(lex.matches("[Number]*")) 
+						{
+							tokensOutput.add("(NUMBER)");
+						}
+						
+						if(lex.matches("[Real_Number]*")) 
+						{
+							tokensOutput.add("(REAL_NUMBER)");
+						}
+						
+						if(lex.matches("[Boolean]*")) 
+						{
+							tokensOutput.add("(BOOLEAN)");
+						}
+						
+						// Check for boolean value
+						if(lex.matches("[[Tt]true|[Ff]false]*")) 
+						{
+							tokensOutput.add("!VALUE_BOOLEAN");
+						}
+						
 					}
 					
-					// Check for Integer
-					else if(lex.matches(".*[0-9]")) 
+					// Check for integer_value
+					else if(lex.matches("[+-]?[0-9][0-9]*")) 
 					{
-						System.out.println(lex + " => " + "Integer");
+						tokensOutput.add("!VALUE_NUMBER");
+					}
+					
+					// Check for float_value
+					else if(lex.matches("[+-]?[0-9]+(\\.[0-9]+)?([Ee][+-]?[0-9]+)?")) 
+					{
+						tokensOutput.add("!VALUE_REAL_NUMBER");
 					}
 					
 					// Check for special character
@@ -66,35 +98,59 @@ public class Lexical
 					{
 						if(lex.matches(".*[\\+]")) 
 						{
-							System.out.println(lex + " => " + "ADD_OP");
+							tokensOutput.add("[ADD_OP]");
 						}
 						else if(lex.matches(".*[-]")) 
 						{
-							System.out.println(lex + " => " + "SUB_OP");
+							tokensOutput.add("[SUB_OP]");
 						}
 						else if(lex.matches(".*[\\*]")) 
 						{
-							System.out.println(lex + " => " + "MUL_OP");
+							tokensOutput.add("[MUL_OP]");
 						}
 						else if(lex.matches(".*[/]")) 
 						{
-							System.out.println(lex + " => " + "DIV_OP");
+							tokensOutput.add("[DIV_OP]");
 						}
 						else if(lex.matches(".*[%]")) 
 						{
-							System.out.println(lex + " => " + "MOD_OP");
+							tokensOutput.add("[MOD_OP]");
 						}
-						else
-							System.out.println(lex + " => " + "Special Character");
+						
 					}
 				}
 				
 			}
+			
+			/* These two following lines are just to format the output file 
+			*  Except the forEach which used to iterate through the ArrayList 
+			*/
+			
+			writeToFile.println("***************** TOKENS *****************");
+			int x = 2;
+			
+			for(String s : tokensOutput) 
+			{
+				if(x % 2 == 0) 
+				{
+					writeToFile.println("-> " + s);
+				}
+				else
+					writeToFile.println(s);
+				x++;
+			}
+			
+			// Close after writing process done
+			writeToFile.close();
 		}
+		
 		
 		catch(FileNotFoundException e) 
 		{
 			System.out.println("File not found!");
 		}
+		
+		// Return the file with tokens
+		return tokens;
 	}
 }
